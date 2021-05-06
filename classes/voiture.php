a1d962b07e2bd6d5c5a234be1ef41768630ee5cd
Raphaël Oester <raphael.oester@ynov.com>
2021-05-06 10:44:59 +0200

    affichage des voitures avec image

--- classes/voiture.php
+++ classes/voiture.php
@@ -5,14 +5,16 @@
         private $_puissance;
         private $_modele;
         private $_km;
+        private $_img;
 
         //constructeur
         //permet d'initialiser chaque instance de l'objet créé
-        public function __construct($marque, $puissance, $modele, $km){
+        public function __construct($marque, $puissance, $modele, $km, $img){
             $this->setMarque($marque);
             $this->setPuissance($puissance);
             $this->setModele($modele);
             $this->setKm($km);
+            $this->setImg($img);
         }
 
         //GETTERS (accesseurs)
@@ -21,13 +23,16 @@
             return $this->_marque;
         }
         public function getPuissance(){
-            return $this->_Puissance;
+            return $this->_puissance;
         }
         public function getModele(){
-            return $this->_Modele;
+            return $this->_modele;
         }
         public function getKm(){
-            return $this->_Km;
+            return $this->_km;
+        }
+        public function getImg(){
+            return $this->_img;
         }
 
 
@@ -54,6 +59,12 @@
             if(is_integer($km) && $km >= 0)
             {
                 $this->_km = $km;
+            }
+        }
+        public function setImg($img){
+            if($img != "")
+            {
+                $this->_img = $img;
             }
         }
     }
--- index.php
+++ index.php
@@ -1,10 +1,37 @@
-<?php 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-    require("voiture.php");
+    <title>Document</title>
+</head>
+<body>
+    <?php 
 
-    $voiture1 = new Voiture("audi", "50CV", "A3", 4000);
+    require("classes/voiture.php");
 
-    echo $voiture1->getMarque();
-    var_dump($voiture1);
+    $voiture1 = new Voiture("Audi", "50CV", "A3", 4000, 'img/audi_a3.jpg');
+    $voiture2 = new Voiture("Opel", "70CV", "1900GT", 70000, 'img/opel_1900GT.jpg');
+    $voiture3 = new Voiture("Chevrolet", "250CV", "Camaro", 35000, 'img/chevrolet_camaro.jpg');
 
-?>
+
+    $voitures = array($voiture1, $voiture2, $voiture3);
+    foreach ($voitures as $voiture) {
+        ?>
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="<?php echo $voiture->getImg()?>" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text"><strong><?php echo $voiture->getMarque()." ".$voiture->getModele()?></strong></p>
+                    <p> <?php echo $voiture->getPuissance()?></p>
+                    <p> <?php echo $voiture->getKm() ?> KM</p>
+                </div>
+            </div>
+        <?php
+    }
+    ?>
+</body>
+</html>
+
