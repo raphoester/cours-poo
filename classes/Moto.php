41c112c6a1255e3e77846abf4b447d266c98c095
Raphaël Oester <raphael.oester@ynov.com>
2021-05-10 11:35:39 +0200

    classes moto et voiture

--- classes/Moto.php
+++ classes/Moto.php
@@ -1,5 +1,41 @@
 <?php 
-    class Moto extends Vehicule{
-        
+class Moto extends Vehicule{
+    private $_nbreRoues;
+    private $_type;
+
+    public function __construct($nbreRoues="", $type="", $id="", $marque="", $puissance="", $modele="", $km="", $img="", $boite=""){
+        parent::__construct($id, $marque, $puissance, $modele, $km, $img, $boite);
+        $this->setnbreRoues($nbreRoues);
+        $this->setType($type);
     }
+
+    public function roueArriere($duree){
+        return "Vr".str_repeat("o", $duree/2).str_repeat("u", $duree/2)."m !";
+    }
+
+    public function getNbreRoues(){
+        return $this->_nbreRoues;
+    }
+    public function getType(){
+        return $this->_type;
+    }
+
+    public function setNbreRoues($nbreRoues){
+        try{
+            $nbreRoues = intval($nbreRoues);
+        }finally{}
+        if(is_integer($nbreRoues) && $nbreRoues >= 2 && $nbreRoues <= 4){
+            $this->_nbreRoues = $nbreRoues;
+        }
+    }
+
+    public function setType($type){
+        try{
+            $type = strval($type);
+        }finally{}
+        if(is_string($type)){
+            $this->_type = $type;
+        }
+    }
+}
 ?>
--- classes/voiture.php
+++ classes/voiture.php
@@ -1,5 +1,40 @@
 <?php 
-    class Voiture extends Vehicule{
-        
+class Voiture extends Vehicule{
+    private $_nbrePortes;
+    private $_decapotable;
+
+    public const DECAPOTABLE = 1;
+    public const CAPOT_FIXE = 0;
+
+    public function __construct($nbrePortes="", $decapotable="", $id="", $marque="", $puissance="", $modele="", $km="", $img="", $boite=""){
+        parent::__construct($id, $marque, $puissance, $modele, $km, $img, $boite);
+        $this->setnbrePortes($nbrePortes);
+        $this->setDecapotable($decapotable);
     }
+
+    public function getNbrePortes(){
+        return $this->_nbrePortes;
+    }
+    public function getDecapotable(){
+        return $this->_decapotable;
+    }
+
+    public function setNbrePortes($nbrePortes){
+        try{
+            $nbrePortes = intval($nbrePortes);
+        }finally{}
+        if(is_integer($nbrePortes) && $nbrePortes >= 2 && $nbrePortes <= 9){
+            $this->_nbrePortes = $nbrePortes;
+        }
+    }
+
+    public function setDecapotable($decapotable){
+        try{
+            $decapotable = boolval($decapotable);
+        }finally{}
+        if(is_bool($decapotable)){
+            $this->_decapotable = $decapotable;
+        }
+    }
+}
 ?>
